f155c6aa6399707fa0fa89f6607ddc1f21a7e91c
KaueSiqueira <kaue54150@gmail.com>
2026-01-23 11:51:00 -0300

    Inicio do projeto - configurações iniciais

---
+++ hub_one/src/main/java/forum/hub_one/HubOneApplication.java
@@ -0,0 +1,13 @@
+package forum.hub_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubOneApplication.class, args);
+	}
+
+}
---
+++ hub_one/src/test/java/forum/hub_one/HubOneApplicationTests.java
@@ -0,0 +1,13 @@
+package forum.hub_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
